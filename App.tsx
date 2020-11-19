90f19a0ac8ecdfbb6890a3b61fcbb9ba4000ca29
sushilkumar50 <bhardwajkumar50@gmail.com>
2020-11-20 01:57:30 +0530

    creating src folder and moving all file into it

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import GameDealsScreen from "./screens/GameDealsScreen";
-import DetailedDealScreen from "./screens/DetailedDealScreen";
+import GameDealsScreen from "./src/screens/GameDealsScreen";
+import DetailedDealScreen from "./src/screens/DetailedDealScreen";
 
-import { ROUTES, APP_TITLE } from "./constants/app-wide-contants";
+import { ROUTES, APP_TITLE } from "./src/constants/app-wide-contants";
 
 const navigator = createStackNavigator(
   {
