fda044532a8d648ba9f28d4cdb41190dde7b1ebf
sushilkumar50 <bhardwajkumar50@gmail.com>
2020-11-20 22:04:38 +0530

    error message styling

--- src/components/ErrorMessageComponent.tsx
+++ src/components/ErrorMessageComponent.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
 import { Text, StyleSheet } from "react-native";
-import PropTypes, { InferProps } from "prop-types";
 
 interface Props {
   message: string;
 }
 export default function ErrorMessageComponent({ message }: Props) {
-  return <Text>{message}</Text>;
+  return <Text style={styles.errorMessage}>{message}</Text>;
 }
+
+const styles = StyleSheet.create({
+  errorMessage: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#394867",
+    alignSelf: "center",
+    marginTop: 50,
+  },
+});
